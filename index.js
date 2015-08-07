b49c08ef20fb39907e35a5747efcf12b9e164a4e
silverwind <me@silverwind.io>
2015-08-07 23:34:35 +0200

    Update all the things, reindent

--- index.js
+++ index.js
@@ -1,127 +1,127 @@
 "use strict";
 
-var extend   = require("util-extend"),
-    events   = require("events"),
-    net      = require("net"),
-    util     = require("util");
+var extend = require("util-extend");
+var events = require("events");
+var net    = require("net");
+var util   = require("util");
 
 var Tcpie = function (host, port, opts) {
-    if (typeof host !== "string")
-        throw new Error("host is required");
+  if (typeof host !== "string")
+    throw new Error("host is required");
 
-    if (typeof port === "undefined")
-        port = 80;
+  if (typeof port === "undefined")
+    port = 80;
 
-    this.host = host;
-    this.port = port;
+  this.host = host;
+  this.port = port;
 
-    this.opts = extend({
-        interval: 1000,
-        timeout : 3000,
-        count   : Infinity
-    }, opts);
+  this.opts = extend({
+    interval: 1000,
+    timeout : 3000,
+    count   : Infinity
+  }, opts);
 
-    this.stats = {
-        sent   : 0,
-        success: 0,
-        failed : 0
-    };
+  this.stats = {
+    sent   : 0,
+    success: 0,
+    failed : 0
+  };
 };
 
 util.inherits(Tcpie, events.EventEmitter);
 
 Tcpie.prototype.start = function start() {
-    var self = this;
+  var self = this;
 
-    if (self.stats.sent >= self.opts.count) return;
+  if (self.stats.sent >= self.opts.count) return;
 
-    self._next = setTimeout(start.bind(self), self.opts.interval);
+  self._next = setTimeout(start.bind(self), self.opts.interval);
 
-    var socket    = new net.Socket(),
-        startTime = now(),
-        done      = false;
+  var socket    = new net.Socket();
+  var startTime = now();
+  var done      = false;
 
-    socket.setTimeout(self.opts.timeout);
-    socket.on("timeout", function () {
-        if (!done) {
-            done = true;
-            self.stats.sent++;
-            self.stats.failed++;
-            self.emit("timeout", addDetails(self, this));
-            socket.destroy();
-            checkEnd(self);
-        }
-    });
+  socket.setTimeout(self.opts.timeout);
+  socket.on("timeout", function () {
+    if (!done) {
+      done = true;
+      self.stats.sent++;
+      self.stats.failed++;
+      self.emit("timeout", addDetails(self, this));
+      socket.destroy();
+      checkEnd(self);
+    }
+  });
 
-    socket.on("error", function (err) {
-        if (!done) {
-            done = true;
-            self.stats.sent++;
-            self.stats.failed++;
-            self.emit("error", err, addDetails(self, this));
-            socket.destroy();
-            checkEnd(self);
-        }
-    });
+  socket.on("error", function (err) {
+    if (!done) {
+      done = true;
+      self.stats.sent++;
+      self.stats.failed++;
+      self.emit("error", err, addDetails(self, this));
+      socket.destroy();
+      checkEnd(self);
+    }
+  });
 
-    socket.connect(self.port, self.host, function () {
-        if (!done) {
-            done = true;
-            self.stats.sent++;
-            self.stats.success++;
-            self.stats.rtt = (now() - startTime) / 1e6;
-            self.emit("connect", addDetails(self, this));
-            socket.end();
-            checkEnd(self);
-        }
-    });
+  socket.connect(self.port, self.host, function () {
+    if (!done) {
+      done = true;
+      self.stats.sent++;
+      self.stats.success++;
+      self.stats.rtt = (now() - startTime) / 1e6;
+      self.emit("connect", addDetails(self, this));
+      socket.end();
+      checkEnd(self);
+    }
+  });
 
-    return self;
+  return self;
 };
 
 module.exports = function (host, port, opts) {
-    return new Tcpie(host, port, opts);
+  return new Tcpie(host, port, opts);
 };
 
 // add details to stats object
 function addDetails(self, socket) {
-    var ret = self.stats;
+  var ret = self.stats;
 
-    ret.target = {
-        host: self.host,
-        port: self.port
-    };
+  ret.target = {
+    host: self.host,
+    port: self.port
+  };
 
-    ret.socket = {
-        localAddress  : socket.localAddress,
-        localPort     : socket.localPort,
-        remoteAddress : socket.remoteAddress,
-        remotePort    : socket.remotePort
-    };
+  ret.socket = {
+    localAddress  : socket.localAddress,
+    localPort     : socket.localPort,
+    remoteAddress : socket.remoteAddress,
+    remotePort    : socket.remotePort
+  };
 
-    return ret;
+  return ret;
 }
 
 // check end condition
 function checkEnd(self) {
-    if ((self.stats.failed + self.stats.success) >= self.opts.count) {
-        if (self._next)
-            clearTimeout(self._next);
+  if ((self.stats.failed + self.stats.success) >= self.opts.count) {
+    if (self._next)
+      clearTimeout(self._next);
 
-        self.emit("end", {
-            sent   : self.stats.sent,
-            success: self.stats.success,
-            failed : self.stats.failed,
-            target : {
-                host: self.host,
-                port: self.port
-            }
-        });
-    }
+    self.emit("end", {
+      sent   : self.stats.sent,
+      success: self.stats.success,
+      failed : self.stats.failed,
+      target : {
+        host: self.host,
+        port: self.port
+      }
+    });
+  }
 }
 
 // get current timestamp in nanoseconds
 function now() {
-    var hrtime = process.hrtime();
-    return hrtime[0] * 1e9 + hrtime[1];
+  var hrtime = process.hrtime();
+  return hrtime[0] * 1e9 + hrtime[1];
 }
--- tcpie.js
+++ tcpie.js
@@ -9,82 +9,82 @@ process.title = pkg.name;
 // avoid EPIPE on partially consumed streams
 require("epipebomb")();
 
-var args   = require("minimist")(process.argv.slice(2)),
-    chalk  = require("chalk"),
-    net    = require("net"),
-    dns    = require("dns"),
-    stdev  = require("compute-stdev"),
-    tcpie  = require("./");
+var args   = require("minimist")(process.argv.slice(2));
+var chalk  = require("chalk");
+var net    = require("net");
+var dns    = require("dns");
+var stdev  = require("compute-stdev");
+var tcpie  = require("./");
 
-
-var DIGITS_LINE  = 1,
-    DIGITS_STATS = 3,
-    DIGITS_PERC  = 0,
-    DEFAULT_PORT = 80;
+var DIGITS_LINE  = 1;
+var DIGITS_STATS = 3;
+var DIGITS_PERC  = 0;
+var DEFAULT_PORT = 80;
 
 var usage = [
-    "",
-    "    Usage: tcpie [options] host[:port]|url [port|80]",
-    "",
-    "    Options:",
-    "",
-    "      -v, --version       output version",
-    "      -c, --count <n>     number of connects (default: infinite)",
-    "      -i, --interval <n>  wait n seconds between connects (default: 1)",
-    "      -t, --timeout <n>   connection timeout in seconds (default: 3)",
-    "      -T, --timestamp     add timestamps to output",
-    "      -f, --flood         flood mode, connect as fast as possible",
-    "      -C, --no-color      disable color output",
-    "",
-    "    Examples:",
-    "",
-    "      $ tcpie google.com",
-    "      $ tcpie -i .1 8.8.8.8:53",
-    "      $ tcpie -c5 -t.05 aspmx.l.google.com 25",
-    "      $ tcpie -i.2 https://google.com",
-    "",
-    ""].join("\n");
+  "",
+  "    Usage: tcpie [options] host[:port]|url [port|80]",
+  "",
+  "    Options:",
+  "",
+  "      -v, --version       output version",
+  "      -c, --count <n>     number of connects (default: infinite)",
+  "      -i, --interval <n>  wait n seconds between connects (default: 1)",
+  "      -t, --timeout <n>   connection timeout in seconds (default: 3)",
+  "      -T, --timestamp     add timestamps to output",
+  "      -f, --flood         flood mode, connect as fast as possible",
+  "      -C, --no-color      disable color output",
+  "",
+  "    Examples:",
+  "",
+  "      $ tcpie google.com",
+  "      $ tcpie -i .1 8.8.8.8:53",
+  "      $ tcpie -c5 -t.05 aspmx.l.google.com 25",
+  "      $ tcpie -i.2 https://google.com",
+  "",
+  ""
+].join("\n");
 
 if (args.v) {
-    process.stdout.write(pkg.version + "\n");
-    process.exit(0);
+  process.stdout.write(pkg.version + "\n");
+  process.exit(0);
 }
 
 if (!args._.length || args._.length > 2 || (args._[1] && isNaN(parseInt(args._[1], 10)))) {
-    help();
+  help();
 }
 
-var host    = args._[0],
-    opts    = {},
-    port    = parseInt(args._[1], 10),
-    printed = false,
-    rtts    = [],
-    stats;
+var host    = args._[0];
+var opts    = {};
+var port    = parseInt(args._[1], 10);
+var printed = false;
+var rtts    = [];
+var stats;
 
 if (typeof host !== "string") {
-    help();
+  help();
 }
 
 // host:port syntax
 var matches = /^(.+):(\d+)$/.exec(host);
 if (matches && matches.length === 3 && !port) {
-    host = matches[1];
-    port = matches[2];
+  host = matches[1];
+  port = matches[2];
 }
 
 // url syntax
 if (/.+:\/\/.+/.test(host)) {
-    var url = require("url").parse(host);
-    var proto = url.protocol.replace(":", "");
-    host = url.host;
-    port = url.port || require("port-numbers").getPort(proto).port;
-    if (!port) {
-        writeLine(chalk.red("ERROR:"), "Unknown protocol '" + proto + "'");
-        process.exit(1);
-    }
-    if (!host) {
-        writeLine(chalk.red("ERROR:"), "Missing host in '" + host + "'");
-    }
+  var url = require("url").parse(host);
+  var proto = url.protocol.replace(":", "");
+  host = url.host;
+  port = url.port || require("port-numbers").getPort(proto).port;
+  if (!port) {
+    writeLine(chalk.red("ERROR:"), "Unknown protocol '" + proto + "'");
+    process.exit(1);
+  }
+  if (!host) {
+    writeLine(chalk.red("ERROR:"), "Missing host in '" + host + "'");
+  }
 }
 
 
@@ -97,143 +97,143 @@ if (args.C || args.color === false) chalk.enabled = false;
 
 // Do a DNS lookup and start the connects
 if (!net.isIP(host)) {
-    dns.lookup(host, function (err, address) {
-        if (!err) {
-            printStart(host, address, port);
-            run(host, port, opts);
-        } else {
-            if (err.code === "ENOTFOUND")
-                writeLine(chalk.red("ERROR:"), "Host '" + host + "' not found");
-            else
-                writeLine(chalk.red("ERROR:"), err.code, err.syscall || "");
-            process.exit(1);
-        }
-    });
+  dns.lookup(host, function (err, address) {
+    if (!err) {
+      printStart(host, address, port);
+      run(host, port, opts);
+    } else {
+      if (err.code === "ENOTFOUND")
+        writeLine(chalk.red("ERROR:"), "Host '" + host + "' not found");
+      else
+        writeLine(chalk.red("ERROR:"), err.code, err.syscall || "");
+      process.exit(1);
+    }
+  });
 } else {
-    printStart(host, host, port);
-    run(host, port, opts);
+  printStart(host, host, port);
+  run(host, port, opts);
 }
 
 function run(host, port, opts) {
-    var pie = tcpie(host, port, opts);
+  var pie = tcpie(host, port, opts);
 
-    pie.on("error", function (err, data) {
-        stats = data;
-        writeLine(
-            chalk.red("error connecting to", data.target.host + ":" + data.target.port),
-            "seq=" + data.sent,
-            "error=" + chalk.red(err.code)
-        );
+  pie.on("error", function (err, data) {
+    stats = data;
+    writeLine(
+      chalk.red("error connecting to", data.target.host + ":" + data.target.port),
+      "seq=" + data.sent,
+      "error=" + chalk.red(err.code)
+    );
+  });
+
+  pie.on("connect", function (data) {
+    stats = data;
+    rtts.push(data.rtt);
+    writeLine(
+      chalk.green("connected to", data.target.host + ":" + data.target.port),
+      "seq=" + data.sent,
+      "srcport=" + data.socket.localPort,
+      "time=" + colorRTT(data.rtt.toFixed(DIGITS_LINE))
+    );
+  });
+
+  pie.on("timeout", function (data) {
+    stats = data;
+    writeLine(
+      chalk.red("timeout connecting to", data.target.host + ":" + data.target.port),
+      "seq=" + data.sent,
+      data.socket.localPort && "srcport=" + data.socket.localPort
+    );
+  });
+
+  if (process.stdin.isTTY) {
+    process.stdin.setRawMode(true);
+    process.stdin.on("data", function (bytes) {
+      // http://nemesis.lonestar.org/reference/telecom/codes/ascii.html
+      if (
+        bytes[0] === 3  || // SIGINT
+        bytes[0] === 4  || // EOF
+        bytes[0] === 26 || // SIGTSTP
+        bytes[0] === 28    // SIGQUIT
+        ) {
+        process.exit();
+      }
     });
+  } else {
+    process.on("SIGINT", process.exit);
+    process.on("SIGQUIT", process.exit);
+    process.on("SIGTERM", process.exit);
+    process.on("SIGTSTP", process.exit);
+  }
 
-    pie.on("connect", function (data) {
-        stats = data;
-        rtts.push(data.rtt);
-        writeLine(
-            chalk.green("connected to", data.target.host + ":" + data.target.port),
-            "seq=" + data.sent,
-            "srcport=" + data.socket.localPort,
-            "time=" + colorRTT(data.rtt.toFixed(DIGITS_LINE))
-        );
-    });
-
-    pie.on("timeout", function (data) {
-        stats = data;
-        writeLine(
-            chalk.red("timeout connecting to", data.target.host + ":" + data.target.port),
-            "seq=" + data.sent,
-            data.socket.localPort && "srcport=" + data.socket.localPort
-        );
-    });
-
-    if (process.stdin.isTTY) {
-        process.stdin.setRawMode(true);
-        process.stdin.on("data", function (bytes) {
-            // http://nemesis.lonestar.org/reference/telecom/codes/ascii.html
-            if (
-                bytes[0] === 3  || // SIGINT
-                bytes[0] === 4  || // EOF
-                bytes[0] === 26 || // SIGTSTP
-                bytes[0] === 28    // SIGQUIT
-                ) {
-                process.exit();
-            }
-        });
-    } else {
-        process.on("SIGINT", process.exit);
-        process.on("SIGQUIT", process.exit);
-        process.on("SIGTERM", process.exit);
-        process.on("SIGTSTP", process.exit);
-    }
-
-    process.on("exit", printEnd);
-    pie.on("end", printEnd).start();
+  process.on("exit", printEnd);
+  pie.on("end", printEnd).start();
 }
 
 function printStart(host, address, port) {
-    writeLine(pkg.name.toUpperCase(), host, "(" + address + ")", "port", String(port));
+  writeLine(pkg.name.toUpperCase(), host, "(" + address + ")", "port", String(port));
 }
 
 function printEnd() {
-    var sum = 0, min = Infinity, max = 0, avg, dev;
+  var sum = 0, min = Infinity, max = 0, avg, dev;
 
-    if (printed)
-        process.exit(0);
-
-    if (stats && stats.sent > 0) {
-        rtts.forEach(function (rtt) {
-            if (rtt <= min) min = rtt.toFixed(DIGITS_STATS);
-            if (rtt >= max) max = rtt.toFixed(DIGITS_STATS);
-            sum += rtt;
-        });
-
-        avg = (sum / rtts.length).toFixed(DIGITS_STATS);
-        dev = stdev(rtts).toFixed(DIGITS_STATS);
-
-        if (min === Infinity) min = "0";
-        if (isNaN(avg)) avg = "0";
-
-        printed = true;
-
-        writeLine("\n---", host, pkg.name + " statistics", "---",
-                  "\n" + stats.sent, "handshakes attempted,", stats.success || "0", "succeeded,",
-                  ((stats.failed / stats.sent) * 100).toFixed(DIGITS_PERC) + "% failed",
-                  "\nrtt min/avg/max/stdev =", min + "/" + avg + "/" + max + "/" + dev, "ms");
-    }
+  if (printed)
     process.exit(0);
+
+  if (stats && stats.sent > 0) {
+    rtts.forEach(function (rtt) {
+      if (rtt <= min) min = rtt.toFixed(DIGITS_STATS);
+      if (rtt >= max) max = rtt.toFixed(DIGITS_STATS);
+      sum += rtt;
+    });
+
+    avg = (sum / rtts.length).toFixed(DIGITS_STATS);
+    dev = stdev(rtts).toFixed(DIGITS_STATS);
+
+    if (min === Infinity) min = "0";
+    if (isNaN(avg)) avg = "0";
+
+    printed = true;
+
+    writeLine("\n---", host, pkg.name + " statistics", "---",
+          "\n" + stats.sent, "handshakes attempted,", stats.success || "0", "succeeded,",
+          ((stats.failed / stats.sent) * 100).toFixed(DIGITS_PERC) + "% failed",
+          "\nrtt min/avg/max/stdev =", min + "/" + avg + "/" + max + "/" + dev, "ms");
+  }
+  process.exit(0);
 }
 
 function colorRTT(rtt) {
-    return chalk[rtt >= 150 ? "red" : rtt >= 75 ? "yellow" : "green"](rtt) + " ms";
+  return chalk[rtt >= 150 ? "red" : rtt >= 75 ? "yellow" : "green"](rtt) + " ms";
 }
 
 function writeLine() {
-    var arg = [].slice.call(arguments), stream;
-    arg = arg.filter(function (string) { return Boolean(string); });
-    if ((args.timeout || args.t) && arg[0][0] !== "\n") arg.unshift(timestamp());
-    arg.push("\n");
-    stream = (process.stdout._type === "pipe" && printed) ? process.stderr : process.stdout;
-    stream.write(arg.join(" "));
+  var arg = [].slice.call(arguments), stream;
+  arg = arg.filter(function (string) { return Boolean(string); });
+  if ((args.timeout || args.t) && arg[0][0] !== "\n") arg.unshift(timestamp());
+  arg.push("\n");
+  stream = (process.stdout._type === "pipe" && printed) ? process.stderr : process.stdout;
+  stream.write(arg.join(" "));
 }
 
 function help() {
-    process.stdout.write(usage);
-    process.exit(1);
+  process.stdout.write(usage);
+  process.exit(1);
 }
 
 function secondsToMs(s) {
-    return (parseFloat(s) * 1000);
+  return (parseFloat(s) * 1000);
 }
 
 function timestamp() {
-    var now = new Date(), day = now.getDate(), month = now.getMonth() + 1,
-        year = now.getFullYear(), hrs = now.getHours(), mins = now.getMinutes(),
-        secs = now.getSeconds();
+  var now = new Date(), day = now.getDate(), month = now.getMonth() + 1;
+  var year = now.getFullYear(), hrs = now.getHours(), mins = now.getMinutes();
+  var secs = now.getSeconds();
 
-    if (month < 10) month = "0" + month;
-    if (day   < 10) day   = "0" + day;
-    if (hrs   < 10) hrs   = "0" + hrs;
-    if (mins  < 10) mins  = "0" + mins;
-    if (secs  < 10) secs  = "0" + secs;
-    return year + "-"  + month + "-" + day + " " + hrs + ":" + mins + ":" + secs;
+  if (month < 10) month = "0" + month;
+  if (day   < 10) day   = "0" + day;
+  if (hrs   < 10) hrs   = "0" + hrs;
+  if (mins  < 10) mins  = "0" + mins;
+  if (secs  < 10) secs  = "0" + secs;
+  return year + "-"  + month + "-" + day + " " + hrs + ":" + mins + ":" + secs;
 }
